7aef16801869e6d0975b7ffb5bf435b2b69ddf11
Sacha Delanoue <contact@shaac.me>
2016-12-06 23:18:59 +0100

    Small code refactor

--- app/src/main/java/me/shaac/countdownbuzzer/MainActivity.java
+++ app/src/main/java/me/shaac/countdownbuzzer/MainActivity.java
@@ -1,44 +1,40 @@
 package me.shaac.countdownbuzzer;
 
+import android.app.Activity;
 import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) this.findViewById(R.id.button);
         //final MediaPlayer mp = MediaPlayer.create(this, R.raw.soho);
-
-
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
         final Handler handler = new Handler();
-        button.setOnClickListener(new View.OnClickListener(){
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                r.play();
+            }
+        };
 
-            private Runnable lastRunnable = null;
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
                 r.stop();
-                if (lastRunnable != null){
-                    handler.removeCallbacks(lastRunnable);
-                }
-                lastRunnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        r.play();
-                    }
-                };
-                handler.postDelayed(lastRunnable, 8000);
+                handler.removeCallbacks(runnable);
+                handler.postDelayed(runnable, 8000);
             }
         });
     }
