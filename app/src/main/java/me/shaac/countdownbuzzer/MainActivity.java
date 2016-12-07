05453c434a38647f39b3468f6982d43279f573bc
Sacha Delanoue <contact@shaac.me>
2016-12-07 22:13:36 +0100

    Use soundPool to play sound

--- app/src/main/java/me/shaac/countdownbuzzer/MainActivity.java
+++ app/src/main/java/me/shaac/countdownbuzzer/MainActivity.java
@@ -1,12 +1,12 @@
 package me.shaac.countdownbuzzer;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,14 +17,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //final MediaPlayer mp = MediaPlayer.create(this, R.raw.soho);
-        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        final SoundPool soundPool;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            soundPool = new SoundPool.Builder().build();
+        } else {
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        }
+        final int alarm = soundPool.load(this, R.raw.alarm, 1);
         final Handler handler = new Handler();
         final Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                r.play();
+                soundPool.play(alarm, 1, 1, 1, 0, 1);
             }
         };
 
@@ -32,7 +36,6 @@ public class MainActivity extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                r.stop();
                 handler.removeCallbacks(runnable);
                 handler.postDelayed(runnable, 8000);
             }
